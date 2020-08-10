b0cf08fcb32468aade3e9f1ccf384bb00b13211e
Paul-Abel <59267403+Paul-Abel@users.noreply.github.com>
2020-08-10 20:36:51 +0200

    1 version finished without special cases

--- SortingFiles.py
+++ SortingFiles.py
@@ -3,14 +3,14 @@ import os
 
 path = '/Users/abelp/Downloads/'
 os.chdir(path)
-#print(os.listdir())
 
 for file in os.listdir():
     filepath = os.path.join(path, file)
     if (file != 'desktop.ini') and (os.path.isfile(filepath)):
-        #print(file)
         filename, file_extension = os.path.splitext(filepath)
-        #print(file_extension)
-        #print(os.path.exists(os.path.join(path, file_extension)))
         if not os.path.exists(os.path.join(path, file_extension)):
             os.makedirs(file_extension)
+        dest_directory = os.path.join(path, file_extension)
+        print(dest_directory)
+        shutil.move(filepath, dest_directory )
+
