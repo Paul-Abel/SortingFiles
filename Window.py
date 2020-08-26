e7d334ecdc72866663e56a8d8271013210336174
Paul-Abel <59267403+Paul-Abel@users.noreply.github.com>
2020-08-26 16:20:21 +0200

    Starting with GUI

---
+++ Window.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root = Tk()
+
+myButton = Button(root, text='Click me', padx='50', pady='10', fg='blue', bg='#00ffff', state='active', heigh='200')
+myButton.pack()
+
+root.mainloop()
